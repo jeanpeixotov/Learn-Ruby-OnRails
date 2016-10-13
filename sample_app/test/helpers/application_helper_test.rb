659a3c9409c88b830a668d8fe91aa590f1ee1086
jeanvitor06 <jeanvitor_06@hotmail.com>
2016-10-12 22:17:15 -0300

    init commit

---
+++ sample_app/test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+# require 'test_helper'
+#
+# class ApplicationHelperTest < ActionView::TestCase
+#   test "full title helper" do
+#     assert_equal full_title,         FILL_IN
+#     assert_equal full_title("Help"), FILL_IN
+#     assert_equal full_title("Aboutlt"), FILL_IN
+#     assert_equal full_title("Contact"), FILL_IN
+#   end
+# end
